ddd01847a5d85fcad77a9902e8386dd1f703666c
1li <35808942+1li@users.noreply.github.com>
2018-02-23 03:45:38 +0800

    Add files via upload

---
+++ 64tv.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>在线电视</title>
+<meta name="keywords" content=""/>
+<meta name="description" content="" />
+<style>  
+body,html{margin:auto;height:100%;}
+</style>
+</head>
+<body>
+
+
+
+<script>
+var w = '950'; //宽度
+var h = '488'; //高度
+var s = 'yes'; //是否显示滚动条，yes显示，no不显示
+document.write('<iframe width='+w+' height='+h+' src="http://live.64ma.com/tv/live.html" frameborder=0 border=0 marginwidth=0 marginheight=0 scrolling='+s+'></iframe>');
+</script>
+</body>
+</html>
